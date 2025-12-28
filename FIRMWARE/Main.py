674a26236d98b823f1df3eac58244f1e3eb145b3
AbhinavKM784 <abhinavkm784@gmail.com>
2025-12-28 12:10:25 +0530

    Add README

---
+++ FIRMWARE/Main.py
@@ -0,0 +1,67 @@
+# You import all the IOs of your board
+import board
+
+# These are imports from the kmk library
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.scanners.keypad import KeysScanner
+from kmk.keys import KC
+from kmk.modules.macros import Press, Release, Tap, Macros
+
+# This is the main instance of your keyboard
+keyboard = KMKKeyboard()
+
+# Add the macro extension
+macros = Macros()
+keyboard.modules.append(macros)
+
+# Define your pins here!
+PINS = [board.D3, board.D4, board.D2, board.D1]
+
+# Tell kmk we are not using a key matrix
+keyboard.matrix = KeysScanner(
+    pins=PINS,
+    value_when_pressed=False,
+)
+
+Mute = KC.MACRO(
+	Press(KC.LCTL)
+	Tap(KC.F4)
+	Delay(10)
+	Release(KC.F4)
+
+)
+NextSong = KC.MACRO(
+	Press(KC.LCTL)
+	Tap(KC.F8)
+	Delay(10)
+	Release(KC.F8)
+
+)
+PrevSong = KC.MACRO(
+	Press(KC.LCTL)
+	Tap(KC.F4)
+	Delay(10)
+	Release(KC.F4)
+
+)
+
+Pause = KC.MACRO(
+	Press(KC.LCTL)
+	Tap(KC.F7)
+	Delay(10)
+	Release(KC.F7)
+
+)
+
+
+
+# Here you define the buttons corresponding to the pins
+# Look here for keycodes: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/keycodes.md
+# And here for macros: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/macros.md
+keyboard.keymap = [
+    [Mute, , Pause, PrevSong,NextSong ]
+]
+
+# Start kmk!
+if __name__ == '__main__':
+    keyboard.go()
